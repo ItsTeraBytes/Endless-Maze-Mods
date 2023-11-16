fea581b054adcb44a4f6d1e75918464b3ec1db4c
TeraBytes <88017555+ItsTeraBytes@users.noreply.github.com>
2023-11-16 12:02:55 +0800

    Update Endless Maze Mod Installer.ps1
    Cleaned it up, added some colors for flare, and added comments.

--- Endless Maze Mod Installer.ps1
+++ Endless Maze Mod Installer.ps1
@@ -1,36 +1,43 @@
-"Endless Maze Mod Installer DEV 1.0"
-"This script will install and uninstall mods for you in the game endless maze"
-"Endless Maze can be found on steam"
-"Mods will only work in Windows!"
+Write-Host "Endless Maze Mod Installer DEV 1.0" -ForegroundColor Cyan
+Write-Host "This script will install and uninstall mods for you in the game endless maze" -ForegroundColor Gray
+Write-Host "Endless Maze can be found on steam" -ForegroundColor Gray
+Write-Host "Mods will only work in Windows!" -ForegroundColor Red
 ""
+
+pause
+Clear
 
 $Game_Path = 'C:\Program Files (x86)\Steam\steamapps\common\EndlessMazex64\windows64\x64'
 
+# Identity game files and ask user when it's missing from the normal steam directory
 if (Test-Path $Game_Path -PathType Container)
 {
-	Write-Host "Endless Maze game detected!"
+	Write-Host "Endless Maze game detected!" -ForegroundColor Green
 } else
 {
 	do {
 		$Game_Path = Read-Host "Endless Maze game not detected. Please type the directory of the steam game"
 	} until (Test-Path $Game_Path -PathType Container)
 
-	"Endless Maze game detected!"
+	Write-Host "Endless Maze game detected!" -ForegroundColor Green
 }
 
+# Check if .txt file exist and if not, create one
 if (-not(Test-Path $Game_Path'\installed_mods_do-not-delete.txt' -PathType Leaf))
 {
 	""
-	"This is the first time you ran this installer"
-	"Creating installed_mods_do-not-delete.txt"
-	"This file is used to track what mods you have installed"
+	Write-Host "This is the first time you ran this installer" -ForegroundColor Gray
+	Write-Host "Creating installed_mods_do-not-modify.txt" -ForegroundColor Gray
+	Write-Host "This file is used to track what mods you have installed" -ForegroundColor Gray
 	""
-	"Do not delete this file, it will break things, this file will not be big anyway"
-	"If you deleted this file, reinstall Endless Maze"
+	Write-Host "Do not delete/modify this file, it will break things, this file will not be big anyway" -ForegroundColor Red
+	Write-Host "If you deleted/modify this file, reinstall Endless Maze" -ForegroundColor Red
 	pause
 	New-Item -Path 	$Game_Path'\installed_mods_do-not-delete.txt' -ItemType File
+	Clear
 }
 
+# Script to download files from GitHub
 function Download-GitHubFolder {
     param (
         [string]$Username = "ItsTeraBytes",
@@ -42,12 +49,12 @@ function Download-GitHubFolder {
 
     $ApiUrl = "https://api.github.com/repos/$Username/$Repository/contents"
 
-    # Append the folder to the API URL if specified
+    # Append the folder to the API URL to specify what folder to download
     if ($Folder -ne "") {
         $ApiUrl += "/$Folder"
     }
 
-    # Add the branch to the API URL
+    # Add the branch name to the API URL
     $ApiUrl += "?ref=$Branch"
 
     function Download-File {
@@ -89,11 +96,11 @@ function Download-GitHubFolder {
     # Download the specified folder
     Download-Folder -Url $ApiUrl -OutputPath $Destination
 
-    Write-Host "Download completed."
+    Write-Host "Download completed." -ForegroundColor Green
     ""
 }
 
-# Function to read text from a URL
+# Read .txt from a GitHub to get list of mods
 function Read-TextFromUrl($url) {
     try {
         $webClient = New-Object System.Net.WebClient
@@ -101,17 +108,19 @@ function Read-TextFromUrl($url) {
         return $content
     }
     catch {
-        Write-Host "Abort! Unable to retrieve mod files! Please make sure you have an internet connection, or GitHub is down..."
+		Clear
+        Write-Host "Abort! Unable to retrieve mod files! Please make sure you have an internet connection, or GitHub is down..." -ForegroundColor Red
 		pause
         return $null
     }
 }
 ""
 "List of mods:"
-# URL of the text file
+
+# URL of GitHub .txt file
 $url = "https://raw.githubusercontent.com/ItsTeraBytes/Endless-maze-mods/main/Mods.txt"
 
-# Read text from the URL
+# Read text from the GitHub
 $fileContent = Read-TextFromUrl $url
 
 # Check if content is retrieved successfully
@@ -122,7 +131,7 @@ if ($fileContent -ne $null) {
     while ($true) {
         # Print the content in a numbered list
         for ($i = 0; $i -lt $lines.Length; $i++) {
-            if (Get-Content $Game_Path'\installed_mods_do-not-delete.txt' | Select-String -Pattern $lines[$i]) {
+            if (Get-Content $Game_Path'\installed_mods_do-not-modify.txt' | Select-String -Pattern $lines[$i]) {
                 Write-Host -NoNewline ("[{0}] {1}" -f ($i + 1), $lines[$i])
                 " (Installed)"
             } else {
@@ -132,7 +141,7 @@ if ($fileContent -ne $null) {
         }
 	    ""
         # Prompt the user for input
-        Write-Host "Type (exit) when you are finished"
+        Write-Host "Type (exit) when you are finished" -ForegroundColor Blue
         $userInput = Read-Host "Enter the mod you want to install/uninstall"
     
         # Validate user input
@@ -143,39 +152,38 @@ if ($fileContent -ne $null) {
             # Get the selected item
             $selectedItem = $lines[$userInput - 1]
 			""
-            Write-Host "Enter (yes) to confirm"
+            Write-Host "Enter (yes) to confirm" -ForegroundColor Blue
             if (Get-Content $Game_Path'\installed_mods_do-not-delete.txt' | Select-String -Pattern $selectedItem) {
                 $confirmation = Read-Host "Uninstall mod $selectedItem"
                 if ($confirmation -eq 'yes') {
 					""
-                    Write-Host "Uninstalling mod: $selectedItem"
+                    Write-Host "Uninstalling mod: $selectedItem" -ForegroundColor Gray
                     ""
-                    Write-Host "Downloading game file(s)..."
+                    Write-Host "Downloading game file(s)..." -ForegroundColor Gray
 
                     Download-GitHubFolder -Branch "main" -Folder "Default/$selectedItem" -Destination $Game_Path
 
                     # Work in progress
-
-                    Write-Host "`nDone!`n`n"
+					pause
                 }
             } else {
                 $confirmation = Read-Host "Install mod $selectedItem"
                 if ($confirmation -eq 'yes') {
 					""
-                    Write-Host "Installing mod: $selectedItem"
+                    Write-Host "Installing mod: $selectedItem" -ForegroundColor Gray
                     ""
-                    Write-Host "Downloading mod file(s)..."
+                    Write-Host "Downloading mod file(s)..." -ForegroundColor Gray
 
                     Download-GitHubFolder -Branch "main" -Folder "Mods/$selectedItem" -Destination $Game_Path
 
                     Add-Content -Path $Game_Path'\installed_mods_do-not-delete.txt' -Value $selectedItem
-
-                    Write-Host "`nDone!`n`n"
+					pause
                 }
             }
         }
         else {
             Write-Host "Invalid input. Please enter a valid number."
         }
+		Clear
     }
 }
